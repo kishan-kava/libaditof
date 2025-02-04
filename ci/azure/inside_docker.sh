9e563ce50ed33833feb68e93ee3bc077bc5b8bbe
SeptimiuVana <vana.septimiu@analog.com>
2025-02-04 12:30:35 +0200

    ci/azure/inside_docker.sh: install dependencies in docker
    Signed-off-by: SeptimiuVana <vana.septimiu@analog.com>

--- ci/azure/inside_docker.sh
+++ ci/azure/inside_docker.sh
@@ -7,21 +7,15 @@ git config --global --add safe.directory /libaditof/protobuf
 project_dir=$1
 pushd ${project_dir}
 
-GLOG_INSTALL_DIR="/aditof-deps/installed/glog"
-PROTOBUF_INSTALL_DIR="/aditof-deps/installed/protobuf"
-WEBSOCKETS_INSTALL_DIR="/aditof-deps/installed/websockets"
-OPENCV_INSTALL_DIR="/aditof-deps/installed/opencv"
 NUM_JOBS=4
 ARGS="$2"
 
 mkdir -p build
 mkdir ../libs
 
-sudo apt-get install libpthread-stubs0-dev
-
 pushd build
-cmake .. ${ARGS} -DCMAKE_PREFIX_PATH="${GLOG_INSTALL_DIR};${PROTOBUF_INSTALL_DIR};${WEBSOCKETS_INSTALL_DIR};${OPENCV_INSTALL_DIR}"
-make -j
+cmake .. ${ARGS}
+make -j4
 popd #build
 
 popd # ${project_dir}
